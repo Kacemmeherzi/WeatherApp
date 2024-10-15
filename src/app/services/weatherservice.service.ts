5601b921f1badf0047f18644498c436a3940450e
kacem <kacemmeherzi52@gmail.com>
2024-10-15 17:45:23 +0100

    getweatherbycity method added

--- src/app/services/weatherservice.service.ts
+++ src/app/services/weatherservice.service.ts
@@ -1,12 +1,23 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class WeatherserviceService {
+export class WeatherService {
 
   constructor(private http: HttpClient) { }
+  private apiUrl = "https://weatherapi-com.p.rapidapi.com/forecast.json" ; 
+   headers = new HttpHeaders({
+    'Content-Type': 'application/json', 
+    'x-rapidapi-key' : '4d8e81d92emsh81398d112f019e1p1e4d06jsnafebeff66d1f',
+    'x-rapidapi-host' : "weatherapi-com.p.rapidapi.com"
 
-  
+  });
+
+  getWeatherByCity(city :string) : Observable<any> {
+    const httpParams = new HttpParams().set("q",city)
+     return this.http.get(this.apiUrl, {headers: this.headers})
+  }
 }
